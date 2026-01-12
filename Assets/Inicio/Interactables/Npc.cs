b3edde4a41b0b732be20f3a8e20a89aacd0a0549
Decripter <andreu.narvaez@estudiantat.upc.edu>
2026-01-12 04:54:03 +0100

    Pre Entrega Final

--- Assets/Inicio/Interactables/Npc.cs
+++ Assets/Inicio/Interactables/Npc.cs
@@ -8,6 +8,27 @@ public class Npc : MonoBehaviour, IInteractable, Imatable
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    public Animator _Animator;
+
+    private CharacterController _Controller;
+    public Transform player;
+    public float detectionRadius = 5f;
+    public float speed = 2f;
+    public SpriteRenderer _SpriteRenderer;
+
+    public float aceleracion = 5f;
+    public float TargetSpeed;
+    public Vector3 currentSpeed;
+    private Vector2 movement;
+
+    [SerializeField]
+    private GroundChecker _GroundChecker;
+
+    private Vector3 fuerza;
+    public float gravedad = -9.8f;
+
+    private bool pacifico;
+
     [SerializeField]
     private GameObject mensaje;
     private Renderer _renderer;
@@ -16,6 +37,7 @@ public class Npc : MonoBehaviour, IInteractable, Imatable
     private bool _interactuable = true;
 
     private bool jugadorestacerca;
+    private bool Muerto;
 
     private void OnEnable()
     {
@@ -26,15 +48,34 @@ public class Npc : MonoBehaviour, IInteractable, Imatable
     {
         WorldManager.Change -= OnChange;
     }
-    private void OnChange(int obj)
+    private void OnChange(int estadoMundo)
     {
         
+        if(!Muerto)
+        {
+            _Animator.SetInteger("estadomundo", estadoMundo);
+        }
+        
+
+
+        if (estadoMundo < 0)
+        {
+            pacifico = false;
+            detectionRadius = 5f;
+        }
+        if (estadoMundo >= 0)
+        {
+            pacifico = true;
+            detectionRadius = 0f;
+            _Animator.SetFloat("movement", 0);
+        }
     }
 
     void Start()
     {
         _renderer = GetComponent<Renderer>();
-        
+        _Controller = GetComponent<CharacterController>();
+
         if (mensaje != null)
         {
             mensaje.SetActive(false);
@@ -48,26 +89,90 @@ public class Npc : MonoBehaviour, IInteractable, Imatable
     {
         if(jugadorestacerca)
         { }
+
+        if (!Muerto)
+        {
+            mover();
+        }
+        AplicarGravedad();
+    }
+
+    private void mover()
+    {
+        float distanciaJugador = Vector2.Distance(transform.position, player.position);
+
+
+        if (distanciaJugador < detectionRadius)
+        {
+            Vector2 direction = (player.position + transform.position).normalized;
+            movement = new Vector2(direction.x, 0);
+        }
+
+        /*if(distanciaJugador > detectionRadius)
+        {
+            //set idle
+        }*/
+
+        else
+        {
+            movement = Vector2.zero;
+        }
+
+        if (movement.x < 0)
+        {
+            _SpriteRenderer.flipX = true;
+        }
+        if (movement.x > 0)
+        {
+            _SpriteRenderer.flipX = false;
+        }
+
+        Vector3 movertarget = (transform.right * movement.x) * speed; //El move original
+
+
+        _Animator.SetFloat("movement", movement.x * speed);
+
+
+        currentSpeed = Vector3.Lerp(currentSpeed, movertarget, aceleracion * Time.deltaTime);
+
+        _Controller.Move(currentSpeed * Time.deltaTime);
+    }
+
+    private void AplicarGravedad()
+    {
+
+        if (_GroundChecker.Tocando && fuerza.y < 0)
+        {
+            fuerza.y = -2f;
+        }
+
+        fuerza.y += gravedad * Time.deltaTime;
+        _Controller.Move(fuerza * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        mensaje.SetActive(true);
-        jugadorestacerca = true;
+        if(!Muerto)
+        {
+            mensaje.SetActive(true);
+            jugadorestacerca = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
+
         mensaje.SetActive(false);
         jugadorestacerca = false;
     }
 
+
     public void Interactuar()
     {
-        if(_interactuable)
+        if(_interactuable && !Muerto)
         {
             WorldManager.instance.Estado(3);
-            Debug.Log("Has hablado con un Npc");
+            Debug.Log("Has ayudado a un Npc");
 
             _renderer.material.color = Color.white;
             _texto.text = "Ayudado!";
@@ -81,7 +186,9 @@ public class Npc : MonoBehaviour, IInteractable, Imatable
         {
             WorldManager.instance.Estado(-3);
             Debug.Log("Has matado a un Npc");
-            Destroy(gameObject);
+            Muerto = true;
+            _Animator.SetBool("Muerto", Muerto);
+            //Destroy(gameObject);
         }
     }
 
